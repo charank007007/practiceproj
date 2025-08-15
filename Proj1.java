0b36cda52420a54c39023de26ca46d38de896b4c
Srikanth <bhimineninagendra2@gmail.com>
2025-08-15 12:43:30 -0500

    added by me

--- Proj1.java
+++ Proj1.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Proj1 {
-}
+public class Proj1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git! This is my first commit.");
+
+        // Let's add a little functionality
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+ }
